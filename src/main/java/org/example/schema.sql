480d4361d528489481dbd762c4f93c3f1481f184
KhondamirChess <hondamirkenjaboev@gmail.com>
2025-11-06 23:18:08 +0500

    update 2

--- src/main/java/org/example/schema.sql
+++ src/main/java/org/example/schema.sql
@@ -8,6 +8,12 @@ CREATE TABLE Book_Table
         ON UPDATE CASCADE;
 );
 
+CREATE TABLE BookCategory
+(
+    category_id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL UNIQUE
+);
+
 CREATE TABLE Reader_Table
 (
     reader_id      SERIAL PRIMARY KEY,
